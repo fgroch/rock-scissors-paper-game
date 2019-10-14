ff31f3e005d6fe5c0f96669ad1c2d807b66e4f74
fgroch <fgroch@gmail.com>
2019-10-14 13:18:19 +0200

    Final application

--- src/tools/blocks/RockScissorsPaper.java
+++ src/tools/blocks/RockScissorsPaper.java
@@ -1,17 +1,83 @@
 package tools.blocks;
 
+import java.util.Random;
+
 /**
  * @author fgroch@gmail.com
  */
 public class RockScissorsPaper {
-    public static void main(String[] args) {
+    private static String SHAPES_TEMPLATE = "PLAYER ONE SHAPE: %s, PLAYER TWO SHAPE: %s. ";
+    private static String WIN_TEMPLATE = "%s beats %s. %s PLAYER WINS.";
+    private static String DEUCE_TEMPLATE = "Deuce.";
 
+    public static void main(String[] args) {
+        Shape firstPlayerShape = Shape.ROCK;
+        Random generator = new Random();
+        for (int i = 0; i < 100; i++) {
+            int ordinal = generator.nextInt(3);
+            Shape secondPlayerShape = Shape.fromOrdinal(ordinal);
+            printResult(firstPlayerShape, secondPlayerShape);
+        }
     }
 
     /**
-     * Prints results of game
+     * Enum that holds Shape and enable check wins over opponents shape. Could be created from [0-2] int range
      */
-    static void printResult() {
+    enum Shape {
+        ROCK,
+        SCISSORS,
+        PAPER;
 
+        /**
+         * Allows to create Shape from [0-2] int range
+         * @param ordinal 0 - ROCK, 1 - SCISSORS, 2 - PAPER
+         * @return Shape corresponding to the order
+         */
+        static Shape fromOrdinal(int ordinal) {
+            if (ordinal ==  ROCK.ordinal()) {
+                return ROCK;
+            } else if (ordinal == SCISSORS.ordinal()) {
+                return SCISSORS;
+            } else if (ordinal == PAPER.ordinal()) {
+                return PAPER;
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        /**
+         * Checks if the shape wins with opponent shape
+         * @param opponentShape Opponent shape to check against
+         * @return true if wins, false otherwise
+         */
+        boolean isWin(Shape opponentShape) {
+            if (this == opponentShape) {
+                return false;
+            } else if (this == ROCK && opponentShape == SCISSORS) {
+                return true;
+            } else if (this == SCISSORS && opponentShape == PAPER) {
+                return true;
+            } else if (this == PAPER && opponentShape == ROCK) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * Prints results of comparsion
+     * @param firstShape first player shape
+     * @param secondShape second player shape
+     */
+    static void printResult(Shape firstShape, Shape secondShape) {
+        System.out.print(String.format(SHAPES_TEMPLATE, firstShape, secondShape));
+        if (firstShape == secondShape) {
+            System.out.println(DEUCE_TEMPLATE);
+        } else if (firstShape.isWin(secondShape)) {
+            System.out.println(String.format(WIN_TEMPLATE, firstShape, secondShape, "FIRST"));
+        } else {
+            System.out.println(String.format(WIN_TEMPLATE, secondShape, firstShape, "SECOND"));
+        }
     }
 }
--- src/tools/blocks/RockScissorsPaperTest.java
+++ src/tools/blocks/RockScissorsPaperTest.java
@@ -4,9 +4,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.*;
 
 public class RockScissorsPaperTest {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream systemOut = System.out;
     private final String PLAYER_ONE_WINS_WITH_ROCK = "PLAYER ONE SHAPE: ROCK, PLAYER TWO SHAPE: SCISSORS. ROCK beats SCISSORS. FIRST PLAYER WINS.\n";
     private final String PLAYER_ONE_LOOSE_WITH_ROCK = "PLAYER ONE SHAPE: ROCK, PLAYER TWO SHAPE: PAPER. PAPER beats ROCK. SECOND PLAYER WINS.\n";
     private final String PLAYER_ONE_WINS_WITH_SCISSORS = "PLAYER ONE SHAPE: SCISSORS, PLAYER TWO SHAPE: PAPER. SCISSORS beats PAPER. FIRST PLAYER WINS.\n";
@@ -16,14 +21,75 @@ public class RockScissorsPaperTest {
 
     @Before
     public void setUp() throws Exception {
+        System.setOut(new PrintStream(out));
     }
 
     @After
     public void tearDown() throws Exception {
+        System.setOut(systemOut);
     }
 
     @Test
     public void shouldPrintAllResults() {
 
+    }
+
+    @Test
+    public void shouldPrintAllWinResult() {
+        String consoleOut = "";
+        RockScissorsPaper.Shape rockShape = RockScissorsPaper.Shape.ROCK;
+        RockScissorsPaper.Shape scissorsShape = RockScissorsPaper.Shape.SCISSORS;
+        RockScissorsPaper.Shape paperShape = RockScissorsPaper.Shape.PAPER;
+
+        RockScissorsPaper.printResult(rockShape, scissorsShape);
+        consoleOut += PLAYER_ONE_WINS_WITH_ROCK;
+        assertEquals(consoleOut, out.toString());
+        RockScissorsPaper.printResult(rockShape, paperShape);
+        consoleOut += PLAYER_ONE_LOOSE_WITH_ROCK;
+        assertEquals(consoleOut, out.toString());
+        RockScissorsPaper.printResult(scissorsShape, paperShape);
+        consoleOut += PLAYER_ONE_WINS_WITH_SCISSORS;
+        assertEquals(consoleOut, out.toString());
+        RockScissorsPaper.printResult(scissorsShape, rockShape);
+        consoleOut += PLAYER_ONE_LOOSE_WITH_SCISSORS;
+        assertEquals(consoleOut, out.toString());
+        RockScissorsPaper.printResult(paperShape, rockShape);
+        consoleOut += PLAYER_ONE_WINS_WITH_PAPER;
+        assertEquals(consoleOut, out.toString());
+        RockScissorsPaper.printResult(paperShape, scissorsShape);
+        consoleOut += PLAYER_ONE_LOOSE_WITH_PAPER;
+        assertEquals(consoleOut, out.toString());
+    }
+
+    @Test
+    public void shouldCreateShapesFromOrdinal() {
+        RockScissorsPaper.Shape shape = RockScissorsPaper.Shape.fromOrdinal(RockScissorsPaper.Shape.ROCK.ordinal());
+        assertEquals(RockScissorsPaper.Shape.ROCK, shape);
+        shape = RockScissorsPaper.Shape.fromOrdinal(RockScissorsPaper.Shape.SCISSORS.ordinal());
+        assertEquals(RockScissorsPaper.Shape.SCISSORS, shape);
+        shape = RockScissorsPaper.Shape.fromOrdinal(RockScissorsPaper.Shape.PAPER.ordinal());
+        assertEquals(RockScissorsPaper.Shape.PAPER, shape);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhileWrongOrdinal() {
+        RockScissorsPaper.Shape.fromOrdinal(4);
+    }
+
+    @Test
+    public void shouldCheckWins() {
+        RockScissorsPaper.Shape rockShape = RockScissorsPaper.Shape.ROCK;
+        RockScissorsPaper.Shape scissorsShape = RockScissorsPaper.Shape.SCISSORS;
+        RockScissorsPaper.Shape paperShape = RockScissorsPaper.Shape.PAPER;
+
+        assertEquals(true, rockShape.isWin(scissorsShape));
+        assertEquals(true, scissorsShape.isWin(paperShape));
+        assertEquals(true, paperShape.isWin(rockShape));
+        assertEquals(false, rockShape.isWin(rockShape));
+        assertEquals(false, scissorsShape.isWin(scissorsShape));
+        assertEquals(false, paperShape.isWin(paperShape));
+        assertEquals(false, rockShape.isWin(paperShape));
+        assertEquals(false, scissorsShape.isWin(rockShape));
+        assertEquals(false, paperShape.isWin(scissorsShape));
     }
 }
